a1cf6aa0f57854c5cf2248d51c90aec5580af458
Matthew Ballard <matballa@ius.edu>
2015-03-21 18:36:47 -0400

    MVC
    Converted Framework to MVC.

---
+++ View.h
@@ -0,0 +1,25 @@
+
+#include "SDL.h"
+
+class View
+{
+public:
+
+	View();
+	void Run();
+
+private:
+	/********************************/
+	/*  User Functions              */
+
+
+	/********************************/
+
+private:
+	/********************************/
+	/*  User Variables              */
+
+	SDL_Window* window;
+
+	/********************************/
+};
